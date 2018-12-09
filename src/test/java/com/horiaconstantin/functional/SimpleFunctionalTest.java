87544121e0c53cbad4b64226e26ab442293c6a23
Horia Constantin <horia.constantin@icloud.com>
2018-12-09 22:04:54 +0100

    small improvements

--- src/test/java/com/horiaconstantin/functional/SimpleFunctionalTest.java
+++ src/test/java/com/horiaconstantin/functional/SimpleFunctionalTest.java
@@ -14,19 +14,19 @@ public class SimpleFunctionalTest {
 
 
     @Test
-    public void name() throws Exception {
+    public void selectFirstCandidate() throws Exception {
         DatabaseTestUtilities databaseTestUtilities = new DatabaseTestUtilities("root", "1234", "mysqljdbc");
         databaseTestUtilities.setMysqlPort(63306);
 
-        try (Connection mySqlConnection = databaseTestUtilities.getMySqlConnection()){
-        Statement statement = mySqlConnection.createStatement();
+        try (Connection mySqlConnection = databaseTestUtilities.getMySqlConnection()) {
+            Statement statement = mySqlConnection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM candidates");
 
-            resultSet.next();
 //                1, 'Carine ', 'Schmitt', '1980-01-01', '(403) 225-5556', 'carine.s@gmail.com\r'
-                assertThat(resultSet.getInt(1), is(1));
-                assertThat(resultSet.getString(2), is("Carine"));
-                assertThat(resultSet.getString(3), is("Schmitt"));
+            resultSet.next();
+            assertThat(resultSet.getInt(1), is(1));
+            assertThat(resultSet.getString(2), is("Carine"));
+            assertThat(resultSet.getString(3), is("Schmitt"));
         }
 
     }
