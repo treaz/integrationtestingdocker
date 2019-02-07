d1cd1b36f0b7bfd60d43d4df17ac0445ff7d0558
Horia Constantin <horia.constantin@icloud.com>
2019-02-07 20:52:50 +0700

    added codepipeline scripts

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+instruction()
+{
+  echo "usage: ./build.sh int-test"
+}
+
+if [ $# -eq 0 ]; then
+  instruction
+  exit 1
+elif [ "$1" = "int-test" ] && [ $# -eq 1 ]; then
+  mvn clean install
+
+  mvn verify
+else
+  instruction
+  exit 1
+fi
